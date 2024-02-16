2210d0298281c0cc2607f412506bd4284b47d345
Prithviraj Verma <158475286+PR-ODINSON@users.noreply.github.com>
2024-02-16 15:20:44 +0530

    Update PDFmerger.py

--- PDFmerger.py
+++ PDFmerger.py
@@ -1,11 +1,21 @@
+# Import necessary libraries
 from pypdf import PdfWriter
 import os
-#Write the path from where you want to input your PDFs
-Files=[file for file in os.listdir() if file.endswith(".pdf")]
+
+# Write the path from where you want to input your PDFs
+# List all files in the current directory with extension .pdf
+Files = [file for file in os.listdir() if file.endswith(".pdf")]
+
+# Create a PdfWriter object for merging PDFs
 Merger = PdfWriter()
 
+# Loop through each PDF file in the directory
 for pdf in Files:
-  Merger.append(pdf)
+    # Append the current PDF file to the PdfWriter object
+    Merger.append(pdf)
 
+# Write the merged PDF to a new file named "merged-pdf.pdf"
 Merger.write("merged-pdf.pdf")
+
+# Close the PdfWriter object
 Merger.close()
